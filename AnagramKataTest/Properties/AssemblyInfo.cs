de58b387adda77ba2743f3a536fbee6876c73a54
DANIEL ELIAS JIMENEZ ROJAS <1084357@est.intec.edu.do>
2020-01-15 01:18:12 -0400

    First Commit

---
+++ AnagramKataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnagramKataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnagramKataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b63ea4c-27cc-40f8-b58c-1f8586cc69f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnagramKataTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramKataTest
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

933faacf6ede286ed67285c4f6c4c898fa4dd8b2
DANIEL ELIAS JIMENEZ ROJAS <1084357@est.intec.edu.do>
2020-01-15 02:42:21 -0400

    5th Test Passed

--- AnagramKataTest/AreAnagramsTest.cs
+++ AnagramKataTest/AreAnagramsTest.cs
@@ -18,5 +18,17 @@ namespace AnagramKataTest
 
             Assert.AreEqual(false, result);
         }
+
+
+        [TestMethod]
+        public void WhenWordsAreNotAnagramsReturnsFalse()
+        {
+            AnagramSelector selector = new AnagramSelector();
+
+            bool result = selector.AreAnagrams("HOLA", "CARLOS");
+            bool expected = false;
+
+            Assert.AreEqual(false, result);
+        }
     }
 }

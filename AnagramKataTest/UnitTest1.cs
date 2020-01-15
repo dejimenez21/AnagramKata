7ca783243fe058a4144be31ea4ce6c5111b4b5b6
DANIEL ELIAS JIMENEZ ROJAS <1084357@est.intec.edu.do>
2020-01-15 10:53:34 -0400

    10th Test Passed.

--- AnagramKataTest/UnitTest1.cs
+++ AnagramKataTest/UnitTest1.cs
@@ -87,5 +87,21 @@ namespace AnagramKataTest
 
         }
 
+
+        [TestMethod]
+        public void WhenIs4WordsWith1SetsOf3AnagramsReturns1LineWith3Words()
+        {
+            //Instanciando clase de selector de anagramas
+            AnagramSelector selector = new AnagramSelector();
+            //Creando lista vacia
+            List<string> words = new List<string>() { "SACO", "SHERFER", "FRESHER", "REFRESH" };
+
+            List<string> result = selector.Group(words);
+            List<string> expected = new List<string>() { "SHERFER FRESHER REFRESH " };
+
+            
+            CollectionAssert.AreEqual(expected, result);
+
+        }
     }
 }
